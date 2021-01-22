4a667872d3929dc3d3753801d81055424f9a9d6e
Anurag Gundappa <anuragg@leotechnosoft.net>
2021-01-22 18:54:11 +0530

    log fetched success

---
+++ README.md
@@ -0,0 +1,12 @@
+# PREREQUISITES
+-----------------------------------------------------------
+1. Download and install Azure CLI
+2. Login into Azure CLI
+3. Create role assignment by using `az ad sp create-for-rbac --name dev-sp-rbac --sdk-auth > local-sp.json`
+4. `dev` can be any string in above command
+5. Get the generated `local-sp.json` and store into `resource` directory.
+
+# DEPLOYMENT
+-------------------------------------------------------------
+
+
--- main.py
+++ main.py
@@ -1,26 +1,28 @@
-from azure.common.client_factory import get_client_from_auth_file
-from azure.mgmt.monitor import MonitorManagementClient
-import datetime
 import os
 
-cred_file = os.path.dirname(__file__) + "/resource/local-sp.json"
-# Get a client for Monitor
-client = get_client_from_auth_file(MonitorManagementClient, auth_path=cred_file)
-print(client)
-# Generate query here
-today = datetime.datetime.now().date()
-print(today)
-filter = "eventTimestamp ge {}".format(today)
-select = ",".join(["eventTimestamp", "eventName", "operationName", "resourceGroupName"])
+from azure.common.client_factory import get_client_from_auth_file
+from azure.mgmt.monitor import MonitorManagementClient
 
-# Grab activity logs
-activity_logs = client.activity_logs.list(filter=filter, select=select)
-print(activity_logs)
-# Print the logs
-for log in activity_logs:
-    print(" ".join([
-        str(log.event_timestamp),
-        str(log.resource_group_name),
-        log.event_name.localized_value,
-        log.operation_name.localized_value
-    ]))
+# Authenticate with auth file
+auth_file = os.path.dirname(__file__) + '/resource/local-sp.json'
+monitor_client = get_client_from_auth_file(MonitorManagementClient, auth_path=auth_file)
+
+# fetch events with monitor cli
+log_filter = "eventTimestamp ge 2021-01-17 15:26:26.896643"
+select = ",".join(["eventTimestamp", "eventName", "operationName", "resourceGroupName"])
+activities = monitor_client.activity_logs.list(filter=log_filter, select=None)
+"""
+ '_attribute_map', '_classify', '_create_xml_node', '_flatten_subtype', '_get_rest_key_parts', '_infer_class_models', 
+ '_subtype_map', '_validation', 'additional_properties', 'as_dict', 'authorization', 'caller', 'category', 'claims', 
+ 'correlation_id', 'description', 'deserialize', 'enable_additional_properties_sending', 'event_data_id', 'event_name', 
+ 'event_timestamp', 'from_dict', 'http_request', 'id', 'is_xml_model', 'level', 'operation_id', 'operation_name', 
+ 'properties', 'resource_group_name', 'resource_id', 'resource_provider_name', 'resource_type', 'serialize', 'status', 
+ 'sub_status', 'submission_timestamp', 'subscription_id', 'tenant_id', 'validate'
+"""
+print(dir(activities))
+"""
+'advance_page', 'async_advance_page', 'async_get', 'current_page', 'get', 'next', 'next_link', 'raw', 'reset'
+"""
+print(activities.raw)
+print(activities.next)
+print(activities.next_link)
