82e65b26ada7ac6b25447b6037056ccc6de1b76a
Anurag Gundappa <anuragg@leotechnosoft.net>
2021-01-26 18:33:23 +0530

    fetching only latest logs

--- azure_log_forwarder.py
+++ azure_log_forwarder.py
@@ -13,18 +13,45 @@ class AzureLogForwarder:
     def __init__(self):
         self.auth_file = os.path.dirname(__file__) + '/resource/local-sp.json'
         self.monitor_client = get_client_from_auth_file(MonitorManagementClient, auth_path=self.auth_file)
-        self.log_filter = "eventTimestamp ge 2021-01-17 15:26:26.896643"
+        self.midnight = datetime.datetime.combine(datetime.datetime.today(), datetime.time())
+        self.log_filter = "eventTimestamp ge %s" % self.midnight
         self.out_file = '/var/log/azure.log'
+        self.event_id_store = os.path.dirname(__file__) + '/resource/event_id_store.data'
+        self.last_event_id = open(self.event_id_store, 'r').readlines()[-1].strip('\n')
 
     def forward(self):
         activities = self.monitor_client.activity_logs.list(filter=self.log_filter, select=None)
-        with open(self.out_file, 'a+') as log_file:
-            for i in activities:
-                jstr = json.dumps(i.as_dict())
-                now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                event = 'Azure - ' + now + " " + jstr + '\n'
-                print(event)
-                log_file.write(event)
+        event_store = []
+
+        # get only administrative events
+        for i in activities:
+            event_dict = i.as_dict()
+            if event_dict['category']['value'] == "Administrative":
+                event_store.append(event_dict)
+
+        # find top event id
+        top_event = event_store[0]
+        top_event_id = top_event['event_data_id']
+
+        # check whether new event are there
+        if top_event_id == self.last_event_id:
+            print("No new events found")
+        else:
+            # if there is new event then remember it
+            with open(self.event_id_store, 'a+') as id_store:
+                id_store.write("%s\n" % top_event_id)
+
+            # process only new event
+            for log in event_store:
+                if log['event_data_id'] == self.last_event_id:
+                    break
+                else:
+                    with open(self.out_file, 'a+') as log_file:
+                        jstr = json.dumps(log)
+                        now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                        event = 'Azure - ' + now + " " + jstr + '\n'
+                        print(event)
+                        log_file.write(event)
 
 
 az = AzureLogForwarder()
