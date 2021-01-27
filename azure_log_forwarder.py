a15ff967d855e3972ec6bbfe93e4dac05ea6b6f4
Anurag Gundappa <anuragg@leotechnosoft.net>
2021-01-27 15:23:31 +0530

    plugin updated

--- azure_log_forwarder.py
+++ azure_log_forwarder.py
@@ -20,35 +20,38 @@ class AzureLogForwarder:
         self.last_event_id = open(self.event_id_store, 'r').readlines()[-1].strip('\n')
 
     def forward(self):
-        activities = self.monitor_client.activity_logs.list(filter=self.log_filter, select=None)
-        event_store = []
+        try:
+            activities = self.monitor_client.activity_logs.list(filter=self.log_filter, select=None)
+            event_store = []
 
-        # get only administrative events
-        for i in activities:
-            event_dict = i.as_dict()
-            if event_dict['category']['value'] == "Administrative":
-                event_store.append(event_dict)
+            # get only administrative events
+            for i in activities:
+                event_dict = i.as_dict()
+                if event_dict['category']['value'] == "Administrative":
+                    event_store.append(event_dict)
 
-        # find top event id
-        top_event = event_store[0]
-        top_event_id = top_event['event_data_id']
+            # find top event id
+            top_event = event_store[0]
+            top_event_id = top_event['event_data_id']
 
-        # check whether new event are there
-        if top_event_id == self.last_event_id:
-            print("No new events found")
-        else:
-            # if there is new event then remember it
-            with open(self.event_id_store, 'a+') as id_store:
-                id_store.write("%s\n" % top_event_id)
+            # check whether new event are there
+            if top_event_id == self.last_event_id:
+                print("No new events found")
+            else:
+                # if there is new event then remember it
+                with open(self.event_id_store, 'a+') as id_store:
+                    id_store.write("%s\n" % top_event_id)
 
-            # process only new event
-            for log in event_store:
-                if log['event_data_id'] == self.last_event_id:
-                    break
-                else:
-                    with open(self.out_file, 'a+') as log_file:
-                        jstr = json.dumps(log)
-                        now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                        event = 'Azure - ' + now + " " + jstr + '\n'
-                        print(event)
-                        log_file.write(event)
+                # process only new event
+                for log in event_store[::-1]:
+                    if log['event_data_id'] == self.last_event_id:
+                        break
+                    else:
+                        with open(self.out_file, 'a+') as log_file:
+                            jstr = json.dumps(log)
+                            now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                            event = 'Azure - ' + now + " " + jstr + '\n'
+                            print(event)
+                            log_file.write(event)
+        except IndexError:
+            print("No new events found!")
