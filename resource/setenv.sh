e1f0f9bb3f0a4ec40e1cd9c223ad8c89534fa216
Anurag Gundappa <anuragg@leotechnosoft.net>
2021-01-27 19:07:39 +0530

    requirements.txt

--- resource/setenv.sh
+++ resource/setenv.sh
@@ -1,7 +1,51 @@
 #!/bin/bash
 
-
+# my machine
 AZURE_SUBSCRIPTION_ID="7d4d6df2-0465-44d1-8742-5c816fd0582b"
 AZURE_TENANT_ID="c53b2ad8-3a9a-4d3a-a7a1-b9215077f2cb"
 AZURE_CLIENT_ID="7b78acbe-451e-4127-9563-b055547a64eb"
 AZURE_CLIENT_SECRET="5e357fb3-7d96-44bf-83c2-2e3ea2cfbc57"
+
+# 199 sensor
+AZURE_SUBSCRIPTION_ID="7d4d6df2-0465-44d1-8742-5c816fd0582b"
+AZURE_TENANT_ID="c53b2ad8-3a9a-4d3a-a7a1-b9215077f2cb"
+AZURE_CLIENT_ID="61e69f55-ba21-4354-9189-dd27c22886a4"
+AZURE_CLIENT_SECRET="QwQne57Hkk03xQkk1W4YCaV-kwrpMk7~qC"
+{
+  "clientId": "61e69f55-ba21-4354-9189-dd27c22886a4",
+  "clientSecret": "QwQne57Hkk03xQkk1W4YCaV-kwrpMk7~qC",
+  "subscriptionId": "7d4d6df2-0465-44d1-8742-5c816fd0582b",
+  "tenantId": "c53b2ad8-3a9a-4d3a-a7a1-b9215077f2cb",
+  "activeDirectoryEndpointUrl": "https://login.microsoftonline.com",
+  "resourceManagerEndpointUrl": "https://management.azure.com/",
+  "activeDirectoryGraphResourceId": "https://graph.windows.net/",
+  "sqlManagementEndpointUrl": "https://management.core.windows.net:8443/",
+  "galleryEndpointUrl": "https://gallery.azure.com/",
+  "managementEndpointUrl": "https://management.core.windows.net/"
+}
+
+# 235 siem
+# rbac not fired yet
+AZURE_SUBSCRIPTION_ID="7d4d6df2-0465-44d1-8742-5c816fd0582b"
+AZURE_TENANT_ID="c53b2ad8-3a9a-4d3a-a7a1-b9215077f2cb"
+AZURE_CLIENT_ID="7b78acbe-451e-4127-9563-b055547a64eb"
+AZURE_CLIENT_SECRET="5e357fb3-7d96-44bf-83c2-2e3ea2cfbc57"
+
+# logger 234
+{
+  "clientId": "5a17e614-f692-4c53-9a52-8acf13df3e41",
+  "clientSecret": "LVbyL5g6OXQmr5D2iU_3zO2qSJ~cAqHyNJ",
+  "subscriptionId": "7d4d6df2-0465-44d1-8742-5c816fd0582b",
+  "tenantId": "c53b2ad8-3a9a-4d3a-a7a1-b9215077f2cb",
+  "activeDirectoryEndpointUrl": "https://login.microsoftonline.com",
+  "resourceManagerEndpointUrl": "https://management.azure.com/",
+  "activeDirectoryGraphResourceId": "https://graph.windows.net/",
+  "sqlManagementEndpointUrl": "https://management.core.windows.net:8443/",
+  "galleryEndpointUrl": "https://gallery.azure.com/",
+  "managementEndpointUrl": "https://management.core.windows.net/"
+}
+
+AZURE_SUBSCRIPTION_ID="7d4d6df2-0465-44d1-8742-5c816fd0582b"
+AZURE_TENANT_ID="c53b2ad8-3a9a-4d3a-a7a1-b9215077f2cb"
+AZURE_CLIENT_ID="5a17e614-f692-4c53-9a52-8acf13df3e41"
+AZURE_CLIENT_SECRET="LVbyL5g6OXQmr5D2iU_3zO2qSJ~cAqHyNJ"
